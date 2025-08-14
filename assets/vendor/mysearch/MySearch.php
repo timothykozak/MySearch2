c7c1a2aedfc33623207f9b28cb6c54f4e03f6e23
timothykozak <timothykozak@yahoo.com>
2025-08-14 10:31:19 -0400

    Removed support for live search.  Removed checking for additional template tokens.  Removed a couple of variables.

--- assets/vendor/mysearch/MySearch.php
+++ assets/vendor/mysearch/MySearch.php
@@ -40,13 +40,8 @@
 // Optional parameters are:
 //      search_dir - the directory in which to begin the search,
 //          please note that this is relative to the location
-//          of this file.  Subdirectories are searched if
-//          $recursive is true.
+//          of this file.  Subdirectories are searched.
 //      filter - file types to search, typically *.html
-//      livesearch - set to nonNull if this is a constantly
-//          updated (live) search
-//      livecount - limit on number of files matched in a
-//          live search
 //
 //  In the returned html the following classes are automatically
 //  inserted without being defined:
@@ -61,16 +56,13 @@
 // from monsterone.com.
 
 // TODO search term of nothing matches everything
-// TODO still an issue with starting directory is not the root
 
 if (!isset($_GET['s'])) {
     die('You must define a search term!');
 }
 
-$highlight = true;  // Highlight results or not
 $search_in = array('html', 'htm');  // Allowable filetypes to search in
 $search_dir = '../..';  // Starting directory, might be overridden by a passed parameter
-$recursive = true;  // Search subdirectories
 define('SIDE_CHARS', 15);
 $file_count = 0;    // The number of files found
 $search_term = mb_strtolower($_GET['s'], 'UTF-8');
@@ -87,17 +79,11 @@ $search_term = preg_replace('/^\/$/', '"/"', $search_term);
 $search_term = preg_replace('/\+/', ' ', $search_term);
 $search_term_length = strlen($search_term);
 
-if (isset($_GET['liveCount'])){
-    $search_live_count = $_GET['liveCount'];
-}
-
 $final_result = array();
 
 $search_filter_init = $_GET['filter'];
 $search_filter = preg_replace("/\*/", ".*", $search_filter_init);
 $search_template = preg_replace('/\+/', ' ', $_GET['template']);
-preg_match_all("/\#\{((?!title|href|token|count)[a-z]*)\}/", $search_template, $template_tokens);
-$template_tokens = $template_tokens[1];
 
 $files = list_files($search_dir);
 
@@ -121,15 +107,7 @@ foreach ($files as $file) {
         $found = strpos_recursive(mb_strtolower($clean_content, 'UTF-8'), $search_term);
 
         $final_result[$file_count]['page_title'][] = $page_title[1];
-        //$final_result[$file_count]['file_name'][] = preg_replace("/^.{3}/", "\\1", $file); // TODO Make normal path
-        //$final_result[$file_count]['file_name'][] = preg_replace("/^\.\.\/\./", "\\1", $file);
         $final_result[$file_count]['file_name'][] = $file;
-    }
-
-    for ($j = 0; $j < count($template_tokens); $j++) {
-        if (preg_match("/\<meta\s+name=[\'|\"]" . $template_tokens[$j] . "[\'|\"]\s+content=[\'|\"](.*)[\'|\"]\>/", $contents, $res)) {
-            $final_result[$file_count][$template_tokens[$j]] = $res[1];
-        }
     }
 
     if ($found && !empty($found)) {
@@ -137,24 +115,14 @@ foreach ($files as $file) {
             $pos = $found[0][$z][1];
             $side_chars = SIDE_CHARS;
             if ($pos < SIDE_CHARS) {
-                $side_chars = $pos;
-                if (isset($_GET['liveSearch']) and $_GET['liveSearch'] != "") {
-                    $pos_end = SIDE_CHARS + $search_term_length + 15;
-                } else {
                     $pos_end = SIDE_CHARS * 9 + $search_term_length;
-                }
             } else {
-                if (isset($_GET['liveSearch']) and $_GET['liveSearch'] != "") {
-                    $pos_end = SIDE_CHARS + $search_term_length + 15;
-                } else {
-                    $pos_end = SIDE_CHARS * 9 + $search_term_length;
-                }
+                $pos_end = SIDE_CHARS * 9 + $search_term_length;
             }
 
             $pos_start = $pos - $side_chars;
             $str = substr($clean_content, $pos_start, $pos_end);
             $result = preg_replace('/' . $search_term . '/ui', '<span class="search">\0</span>', $str);
-            //$result = preg_replace('#'.$search_term.'#ui', '<span class="search">'.$search_term.'</span>', $str);
             $final_result[$file_count]['search_result'][] = $result;
 
         }
@@ -177,10 +145,6 @@ if ($file_count > 0) {
 
 <div class="search-results">
 
-    <?php if (count($final_result) > 0 and isset($_GET['liveSearch']) and $_GET['liveSearch'] != "") {
-        echo "<div class='search-quick-result'>Quick Results</div>";
-    } ?>
-
     <ol class="search-list">
         <?php
         $sum_of_results = 0;
@@ -189,8 +153,6 @@ if ($file_count > 0) {
             if (!empty($final_result[$i]['search_result'][0]) || $final_result[$i]['search_result'][0] !== '') {
                 $match_count++;
                 $sum_of_results += count($final_result[$i]['search_result']);
-                if (isset($_GET['liveSearch']) and $_GET['liveSearch'] != "" and $i >= $search_live_count) {
-                } else {
                     ?>
                     <li class="search-list-item">
 
@@ -201,38 +163,15 @@ if ($file_count > 0) {
                             count($final_result[$i]['search_result'])
                         ];
                         $template = preg_replace(["/#{title}/","/#{href}/","/#{token}/","/#{count}/"],$replacement, $search_template);
-                        for ($k = 0; $k < count($template_tokens); $k++){
-                            if (isset($final_result[$i][$template_tokens[$k]])){
-                                $template = preg_replace("/#{" . $template_tokens[$k] . "}/", $final_result[$i][$template_tokens[$k]], $template);
-                            }else{
-                                $template = preg_replace("/#{" . $template_tokens[$k] . "}/", " ", $template);
-                            }
-                        }
 
                         echo $template; ?>
                     </li>
                     <?php
                 }
-            }
         }
 
         if ($match_count == 0) {
             echo '<li><div class="search-error">No results found for "<span class="search">' . $search_term . '</span>"<div/></li>';
-        }
-        ?>
-        <?php
-        if (isset($_GET['liveSearch']) and $_GET['liveSearch'] != "" and $match_count != 0) {
-            ?>
-            <li class="search-list-item-all">
-                <a href='search-results.html?s=<?php echo $_GET['s']; ?>&amp;filter=<?php echo $search_filter_init; ?>'  class="search-submit">
-                    <?php
-                    echo "See other ";
-                    echo $sum_of_results;
-                    echo $sum_of_results < 2 ? " result on " : " results";
-                    ?>
-                </a>
-            </li>
-            <?php
         }
         ?>
     </ol>
@@ -241,7 +180,7 @@ if ($file_count > 0) {
 <?php
 function list_files($dir)
 {   //lists all the files in the directory given (and sub-directories if it is enabled)
-    global $recursive, $search_in;
+    global $search_in;
 
     $result = array();
     if (is_dir($dir)) {
@@ -249,7 +188,7 @@ function list_files($dir)
             while (($file = readdir($dh)) !== false) {
                 if (!($file == '.' || $file == '..')) { // Not the current or parent directory
                     $file = $dir . '/' . $file;
-                    if (is_dir($file) && $recursive == true && $file != './.' && $file != './..') { // Recursively walk the subdirectories
+                    if (is_dir($file) && $file != './.' && $file != './..') { // Recursively walk the subdirectories
                         $result = array_merge($result, list_files($file));
                     } else if (!is_dir($file)) {
                         if (in_array(get_file_extension($file), $search_in)) {
