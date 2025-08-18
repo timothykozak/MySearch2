e6c05e9f0e6190d38fa395b11b21752dbed91766
timothykozak <timothykozak@yahoo.com>
2025-08-17 20:18:55 -0400

    Removed filter and root_dir.  Renamed s to search_term.  Now gives an error messaged if problem with the HTTP request.

--- assets/vendor/mysearch/GetResult.js
+++ assets/vendor/mysearch/GetResult.js
@@ -10,28 +10,26 @@ function getResult() {
   let searchElement = document.getElementById("search_input");
 
   let searchString = searchElement.value;
-  let filter = '*.html';  // Extensions to examine
-  let searchDir = '../../..';
-  let rootDir = '../../..';  // The site root relative to the location of the search html and search PHP
+  let searchDir = '../../..'; // The root of the site relative to the location of the search html and search PHP
   let template = `<h5 class="search-title"><a target="_top" href="#{href}" class="search-link">#{title}</a></h5><p>...#{token}...</p><p class="match"><em>Terms matched: #{count}</em></p>`;
 
 
   let xhr = new XMLHttpRequest();
   let theURL = 'MySearch.php';
   let request = new URLSearchParams(
-    { s: searchString,        //  key/value pairs are passed as key=value
+    { search_term: searchString,        //  key/value pairs are passed as key=value
       template: template,
-      filter: filter,
-      search_dir: searchDir,
-      root_dir: rootDir});
+      search_dir: searchDir});
 
   xhr.open( 'GET', theURL +'?'+ request.toString(), true );
   // Need to open before assigning events
   xhr.onreadystatechange = ( event ) => { // Respond to all changes
-    if( xhr.readyState === 4 && xhr.status === 200 ) {  // Wait for completion
-      // readyState of 4 = DONE
-      // status of 200 = OK  i.e. The resource has been fetched and transmitted in the message body.
-      resultsElement.innerHTML = xhr.response;
+    if( xhr.readyState === XMLHttpRequest.DONE ) {  // Wait for completion
+      if ( xhr.status === 200 ) { // status of 200 = OK  i.e. The resource has been fetched and transmitted in the message body.
+        resultsElement.innerHTML = xhr.response;
+      } else {  // Could be any number of errors
+        resultsElement.innerHTML = "Server Error = " + xhr.status;
+      }
     }
   };
 
